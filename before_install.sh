ffba8181d58d50b7944610c538651d1108f8add1
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2024-03-03 18:34:54 +0530

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -2,5 +2,5 @@
 
 # Stop the Apache HTTP Server
 service apache2 stop || systemctl stop apache2
-containerid=`docker ps | awk -F " " 'print{1}'`
+containerid=`docker ps | awk -F " " '{print $1}'`
 docker rm -f $containerid
