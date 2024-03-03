e177aa58b2f73be9500630d073746ed0bc6c77a9
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2024-03-03 18:03:58 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# Set permissions for the web server files
 chown -R www-data:www-data /var/www/html
 docker pull bhaskaranil/html_cicd:latest
 docker run -d -p 80:80 bhaskaranil/html_cicd
